82fa051653ecd171f5dd7fe1423014e55bff8444
Murilojms7 <murilojms7@gmail.com>
2024-03-04 16:23:22 -0300

    Spring boot iniciado

---
+++ src/main/java/com/example/SportsJerseyKingdom/SportsJerseyKingdomApplication.java
@@ -0,0 +1,13 @@
+package com.example.SportsJerseyKingdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsJerseyKingdomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsJerseyKingdomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SportsJerseyKingdom/SportsJerseyKingdomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SportsJerseyKingdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsJerseyKingdomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
