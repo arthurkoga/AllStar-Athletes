f74e0b3bada182fd190d54bfd331067d915d46eb
murilojms7 <murilojms7@gmail.com>
2024-03-06 11:26:26 -0300

    delete

--- src/main/java/com/example/SportsJerseyKingdom/Controller/CamisaController.java
+++ src/main/java/com/example/SportsJerseyKingdom/Controller/CamisaController.java
@@ -1,57 +1,71 @@
-package com.example.SportsJerseyKingdom.Controller;
+    package com.example.SportsJerseyKingdom.Controller;
 
-import com.example.SportsJerseyKingdom.Models.Camisa;
+    import com.example.SportsJerseyKingdom.Models.Camisa;
 
-import java.util.ArrayList;
-import java.util.List;
+    import java.util.ArrayList;
+    import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.web.bind.annotation.RequestBody;
+    import org.springframework.web.bind.annotation.RequestMapping;
+    import org.springframework.web.bind.annotation.RequestMethod;
+    import org.springframework.web.bind.annotation.ResponseBody;
+    import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class CamisaController {
-    
-    
-    Logger log = LoggerFactory.getLogger(getClass());
+    @RestController
+    @RequestMapping("/camisa")
 
-    List<Camisa> repository = new ArrayList<>();
-
-    //GET
-    @RequestMapping(method=RequestMethod.GET , path="/camisa")
-    @ResponseBody
-    public List<Camisa> index(){
-        return repository;
-    }
-
-    //POST
-    @RequestMapping(method=RequestMethod.POST ,path="/camisa")
-    @ResponseBody
-    public ResponseEntity<Camisa> create(@RequestBody Camisa camisa){
-        log.info("cadastrando camisa: {}", camisa);
-        repository.add(camisa);
-        return ResponseEntity.status(201).body(camisa);
-    }
-
-    @RequestMapping(method = RequestMethod.GET, path = "/camisa/{id}")
-    @ResponseBody
-    public ResponseEntity<Camisa> get (@PathVariable() Long id) {
-        log.info("buscando camisa com id {}", id);
-        var camisa = repository
-            .stream()
-            .filter(c -> c.id().equals(id)) // filtra por id
-            .findFirst(); //pega o primeiro item 
+    public class CamisaController {
         
-        if (camisa.isEmpty()){
-                return ResponseEntity.status(404).build();
-            }
-                return ResponseEntity.status(200).body(camisa.get());
-    }
+        
+        Logger log = LoggerFactory.getLogger(getClass());
 
-}
+        List<Camisa> repository = new ArrayList<>();
+
+        //GET
+        @GetMapping()
+        public List<Camisa> index(){
+            return repository;
+        }
+
+        //POST
+        @PostMapping()
+        public ResponseEntity<Camisa> create(@RequestBody Camisa camisa){
+            log.info("cadastrando camisa: {}", camisa);
+            repository.add(camisa);
+            return ResponseEntity.status(201).body(camisa);
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Camisa> get (@PathVariable() Long id) {
+            log.info("buscando camisa com id {}", id);
+            var camisa = repository
+                .stream()
+                .filter(c -> c.id().equals(id)) // filtra por id
+                .findFirst(); //pega o primeiro item 
+            
+            if (camisa.isEmpty()){
+                    return ResponseEntity.notFound().build();
+                }
+                    return ResponseEntity.ok(camisa.get());
+        }
+
+        public ResponseEntity<Camisa> destroy(@PathVariable long id){
+            log.info("Apagando acamisa" + id);
+            var camisa = repository
+                .stream()
+                .filter(c -> c.id().equals(id)) // filtra por id
+                .findFirst(); //pega o primeiro item 
+            
+            if (camisa.isEmpty()){
+                    return ResponseEntity.notFound().build();
+                }
+                    return ResponseEntity.ok(camisa.get());
+        }
+    }
